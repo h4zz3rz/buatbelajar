62c0d93b591ad6eb72c505118670077154560b0c
d3jz <cudlaz02@gmail.com>
2025-09-01 18:43:50 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyARnF7pHM9i2xmd2Dh68bVx8Bh9tCOah6I",
+    authDomain: "bisinis-unbaned-testing1.firebaseapp.com",
+    projectId: "bisinis-unbaned-testing1",
+    storageBucket: "bisinis-unbaned-testing1.firebasestorage.app",
+    messagingSenderId: "161325332962",
+    appId: "1:161325332962:web:de5be6347fd8e36f12e1bb",
+    measurementId: "G-BSC9JH7NDY"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
+  export const db = getFirestore(app);
+</script>
