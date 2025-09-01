98a1072c3712f114e81771173ed633da4c11e15f
d3jz <cudlaz02@gmail.com>
2025-09-01 18:47:06 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+<!-- firebase.js -->
 <script type="module">
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
   import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
@@ -7,13 +8,16 @@
     apiKey: "AIzaSyARnF7pHM9i2xmd2Dh68bVx8Bh9tCOah6I",
     authDomain: "bisinis-unbaned-testing1.firebaseapp.com",
     projectId: "bisinis-unbaned-testing1",
-    storageBucket: "bisinis-unbaned-testing1.firebasestorage.app",
+    storageBucket: "bisinis-unbaned-testing1.appspot.com",
     messagingSenderId: "161325332962",
     appId: "1:161325332962:web:de5be6347fd8e36f12e1bb",
     measurementId: "G-BSC9JH7NDY"
   };
 
+  // Init Firebase
   const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
-  export const db = getFirestore(app);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+
+  export { auth, db };
 </script>
